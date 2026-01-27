9d0c8679f4804e0e26c3678efed3e3a6a992fc6f
sammy33440-commits <sammy33440@gmail.com>
2026-01-27 13:47:27 +0100

    Update build_waveshare_rp2040_zero.sh

--- src/boards/build_waveshare_rp2040_zero.sh
+++ src/boards/build_waveshare_rp2040_zero.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-# ON FORCE LA CONFIGURATION PIZERO USBC
+# CONFIGURATION ULTIME WAVESHARE PIZERO USB-C
+# Basé sur GPIO 6 (D+) et GPIO 7 (D-)
+# Horloge forcée à 120MHz pour la stabilité PIO-USB
+
 cmake -G "Unix Makefiles" \
   -DFAMILY=rp2040 \
   -DPICO_BOARD=waveshare_rp2040_zero \
@@ -7,4 +10,5 @@ cmake -G "Unix Makefiles" \
   -DPICO_USB_HOST_DM_PIN=7 \
   -DCONFIG_USB=1 \
   -DPICO_PIO_USB_CLOCK_KHZ=120000 \
+  -DPIO_USB_VBUS_PIN=-1 \
   -B build
