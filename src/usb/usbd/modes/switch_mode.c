095c46e59c6de7149a2772f46f092e6e5d3568ac
sammy33440-commits <sammy33440@gmail.com>
2026-01-27 22:24:46 +0100

    Update switch_mode.c

--- src/usb/usbd/modes/switch_mode.c
+++ src/usb/usbd/modes/switch_mode.c
@@ -5,10 +5,6 @@
 #include "core/buttons.h"
 #include <string.h>
 
-#ifdef HID_KEY_SPACE
-#undef HID_KEY_SPACE
-#endif
-
 static switch_in_report_t switch_report;
 
 static uint8_t convert_dpad_to_hat(uint32_t buttons) {
@@ -16,6 +12,7 @@ static uint8_t convert_dpad_to_hat(uint32_t buttons) {
     uint8_t down = (buttons & JP_BUTTON_DD) ? 1 : 0;
     uint8_t left = (buttons & JP_BUTTON_DL) ? 1 : 0;
     uint8_t right = (buttons & JP_BUTTON_DR) ? 1 : 0;
+
     if (up && right) return SWITCH_HAT_UP_RIGHT;
     if (up && left) return SWITCH_HAT_UP_LEFT;
     if (down && right) return SWITCH_HAT_DOWN_RIGHT;
@@ -34,13 +31,13 @@ static void switch_mode_init(void) {
     switch_report.ly = SWITCH_JOYSTICK_MID;
     switch_report.rx = SWITCH_JOYSTICK_MID;
     switch_report.ry = SWITCH_JOYSTICK_MID;
-    switch_report.vendor = 0;
 }
 
-static bool switch_mode_is_ready(void) { return tud_hid_ready(); }
+static bool switch_mode_is_ready(void) {
+    return tud_hid_ready();
+}
 
 static bool switch_mode_send_report(uint8_t player_index, const input_event_t* event, const profile_output_t* profile_out, uint32_t buttons) {
-    (void)player_index; (void)event;
     switch_report.buttons = 0;
     if (buttons & JP_BUTTON_B1) switch_report.buttons |= SWITCH_MASK_B;
     if (buttons & JP_BUTTON_B2) switch_report.buttons |= SWITCH_MASK_A;
@@ -50,18 +47,13 @@ static bool switch_mode_send_report(uint8_t player_index, const input_event_t* e
     if (buttons & JP_BUTTON_R1) switch_report.buttons |= SWITCH_MASK_R;
     if (buttons & JP_BUTTON_L2) switch_report.buttons |= SWITCH_MASK_ZL;
     if (buttons & JP_BUTTON_R2) switch_report.buttons |= SWITCH_MASK_ZR;
-    if (buttons & JP_BUTTON_S1) switch_report.buttons |= SWITCH_MASK_MINUS;
-    if (buttons & JP_BUTTON_S2) switch_report.buttons |= SWITCH_MASK_PLUS;
-    if (buttons & JP_BUTTON_L3) switch_report.buttons |= SWITCH_MASK_L3;
-    if (buttons & JP_BUTTON_R3) switch_report.buttons |= SWITCH_MASK_R3;
-    if (buttons & JP_BUTTON_A1) switch_report.buttons |= SWITCH_MASK_HOME;
-    if (buttons & JP_BUTTON_A2) switch_report.buttons |= SWITCH_MASK_CAPTURE;
+    
     switch_report.hat = convert_dpad_to_hat(buttons);
     switch_report.lx = profile_out->left_x;
     switch_report.ly = profile_out->left_y;
     switch_report.rx = profile_out->right_x;
     switch_report.ry = profile_out->right_y;
-    switch_report.vendor = 0;
+    
     return tud_hid_report(0, &switch_report, sizeof(switch_in_report_t));
 }
 
@@ -70,12 +62,10 @@ static const uint8_t* switch_mode_get_config_descriptor(void) { return switch_co
 static const uint8_t* switch_mode_get_report_descriptor(void) { return switch_report_descriptor; }
 
 const usbd_mode_t switch_mode = {
-    .name = "Switch",
-    .mode = 5, // USB_OUTPUT_MODE_SWITCH
+    .init = switch_mode_init,
+    .is_ready = switch_mode_is_ready,
+    .send_report = switch_mode_send_report,
     .get_device_descriptor = switch_mode_get_device_descriptor,
     .get_config_descriptor = switch_mode_get_config_descriptor,
     .get_report_descriptor = switch_mode_get_report_descriptor,
-    .init = switch_mode_init,
-    .send_report = switch_mode_send_report,
-    .is_ready = switch_mode_is_ready,
 };
