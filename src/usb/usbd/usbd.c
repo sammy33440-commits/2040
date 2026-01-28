48181f7a8255bd7a4299e370d4d5c83f615edb04
sammy33440-commits <sammy33440@gmail.com>
2026-01-28 01:41:51 +0100

    Update usbd.c

--- src/usb/usbd/usbd.c
+++ src/usb/usbd/usbd.c
@@ -1,55 +1,95 @@
 #include "usbd.h"
 #include "usbd_mode.h"
 
-// CHEMINS RELATIFS (INFAILLIBLES)
-// On remonte d'un dossier (..) pour aller dans descriptors
+// CHEMINS RELATIFS POUR FIXER LA COMPILATION
 #include "../descriptors/hid_descriptors.h"
-#include "../descriptors/switch_descriptors.h"
+#include "../descriptors/xbox_og_descriptors.h"
+#include "../descriptors/xinput_descriptors.h"
+#include "../descriptors/switch_descriptors.h" // Pointera vers ta nouvelle version "Pro"
+#include "../descriptors/ps3_descriptors.h"
+#include "../descriptors/psclassic_descriptors.h"
+#include "../descriptors/ps4_descriptors.h"
+#include "../descriptors/xbone_descriptors.h"
+#include "../descriptors/xac_descriptors.h"
+#include "../descriptors/kbmouse_descriptors.h"
+#include "../descriptors/gc_adapter_descriptors.h"
 
+#include "kbmouse/kbmouse.h"
 #include "tusb.h"
+#include "pico/unique_id.h"
+#include "hardware/watchdog.h"
 #include <string.h>
+#include <stdio.h>
 
-// On déclare uniquement les modes qu'on va utiliser
+// Déclaration de tous les modes externes
 extern const usbd_mode_t hid_mode;
+extern const usbd_mode_t xinput_mode;
 extern const usbd_mode_t switch_mode;
+extern const usbd_mode_t ps3_mode;
+extern const usbd_mode_t psclassic_mode;
+extern const usbd_mode_t ps4_mode;
+extern const usbd_mode_t xid_mode;
+extern const usbd_mode_t xbone_mode;
+extern const usbd_mode_t xac_mode;
+extern const usbd_mode_t kbmouse_mode;
+extern const usbd_mode_t gc_adapter_mode;
 
-// On force le mode Switch
-static usb_output_mode_t output_mode = USB_OUTPUT_MODE_SWITCH; 
+#define USB_MAX_PLAYERS 4
+static input_event_t pending_events[USB_MAX_PLAYERS];
+static bool pending_flags[USB_MAX_PLAYERS] = {false};
+static char usb_serial_str[13];
+static usb_output_mode_t output_mode = USB_OUTPUT_MODE_HID;
+static flash_t flash_settings;
+
 const usbd_mode_t* usbd_modes[USB_OUTPUT_MODE_COUNT] = {0};
 static const usbd_mode_t* current_mode = NULL;
 
 void usbd_register_modes(void) {
+    // On remet TOUS les modes
     usbd_modes[USB_OUTPUT_MODE_HID] = &hid_mode;
+    usbd_modes[USB_OUTPUT_MODE_XINPUT] = &xinput_mode;
     usbd_modes[USB_OUTPUT_MODE_SWITCH] = &switch_mode;
+    usbd_modes[USB_OUTPUT_MODE_PS3] = &ps3_mode;
+    usbd_modes[USB_OUTPUT_MODE_PSCLASSIC] = &psclassic_mode;
+    usbd_modes[USB_OUTPUT_MODE_PS4] = &ps4_mode;
+    usbd_modes[USB_OUTPUT_MODE_XBOX_ORIGINAL] = &xid_mode;
+    usbd_modes[USB_OUTPUT_MODE_XBONE] = &xbone_mode;
+    usbd_modes[USB_OUTPUT_MODE_XAC] = &xac_mode;
+    usbd_modes[USB_OUTPUT_MODE_KEYBOARD_MOUSE] = &kbmouse_mode;
+    usbd_modes[USB_OUTPUT_MODE_GC_ADAPTER] = &gc_adapter_mode;
 }
+
+// ... Le reste des fonctions standard ...
+// Pour gagner de la place ici, garde tes fonctions usbd_init, usbd_task, etc.
+// Mais assure-toi que usbd_init appelle bien usbd_register_modes() !
 
 void usbd_init(void) {
     usbd_register_modes();
-    current_mode = usbd_modes[output_mode];
+    flash_init();
+    if (flash_load(&flash_settings)) {
+        if (flash_settings.usb_output_mode < USB_OUTPUT_MODE_COUNT)
+            output_mode = (usb_output_mode_t)flash_settings.usb_output_mode;
+    }
     
-    // Initialisation Port 0 (Device)
+    // Protection: Si mode inconnu, retour HID
+    if (!usbd_modes[output_mode]) output_mode = USB_OUTPUT_MODE_HID;
+    
+    current_mode = usbd_modes[output_mode];
+
+    pico_get_unique_board_id_string(usb_serial_str, sizeof(usb_serial_str));
     tusb_rhport_init_t dev_init = { .role = TUSB_ROLE_DEVICE, .speed = TUSB_SPEED_AUTO };
     tusb_init(0, &dev_init);
-    
+
     if (current_mode && current_mode->init) current_mode->init();
+    router_set_tap(OUTPUT_TARGET_USB_DEVICE, (void*)0);
 }
 
 void usbd_task(void) {
     tud_task();
     if (current_mode && current_mode->is_ready && current_mode->is_ready()) {
-        usbd_send_report(0);
+         // Logique d'envoi simplifiée
+         // (Tu peux remettre ton code original ici si tu l'as, sinon garde simple)
     }
 }
-
-bool usbd_send_report(uint8_t player_index) {
-    (void)player_index;
-    return true; 
-}
-
-// Callbacks TinyUSB
-uint8_t const *tud_descriptor_device_cb(void) { return current_mode->get_device_descriptor(); }
-uint8_t const *tud_descriptor_configuration_cb(uint8_t index) { (void)index; return current_mode->get_config_descriptor(); }
-uint8_t const *tud_hid_descriptor_report_cb(uint8_t itf) { (void)itf; return current_mode->get_report_descriptor(); }
-void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize) {}
-uint16_t tud_hid_get_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer, uint16_t reqlen) { return 0; }
-uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid) { return NULL; }
+// Ajoute ici les callbacks TinyUSB standard (device_cb, report_cb, etc)
+// qui appellent current_mode->callback()
